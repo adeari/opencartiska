ccb0b834100acae08b67a57e024b66ed07cd2a73
ade <ade_ari_w0@yahoo.com>
2013-10-10 20:14:16 +0700

    fffggrt

---
+++ upload/catalog/language/indonesia/product/testimonial.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title'] 	= 'Testimoni Pelanggan';
+
+$_['text_average']      = 'Nilai Rata-Rata:';
+$_['text_stars']        = '%s dari 5 bintang!';
+$_['text_no_rating']    = 'Tidak ada penilaian';
+
+// Text
+$_['text_error'] 		= 'Testimoni: Tidak ada testimoni!';
+$_['text_pagination'] 	= 'Testimoni {start} t/m {end} dari {total} ({pages} halaman)';
+$_['text_showall'] 	= 'Lihat semua';
+$_['text_write'] 		= 'Tulis Testimoni';
+$_['text_auteur'] 	= ' ( Penulis: %s )';
+$_['text_city'] 		= '<i> dari %s</i>';
+?>
