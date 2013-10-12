9e0fe34b48a10fd901fa71bb2e57a90a8065f335
ade <ade_ari_w0@yahoo.com>
2013-10-12 13:06:11 +0700

    jne dan facebook

---
+++ upload/mimoki/language/indonesia/module/fbconnect.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']       = 'Terhubung ke Facebook ';
+
+// Text
+$_['text_module']         = 'Modul';
+$_['text_success']        = 'Sukses: Koneksi ke Facebook tersimpan!';
+$_['text_content_top']    = 'Bagian Atas';
+$_['text_content_bottom'] = 'Bagian Bawah';
+$_['text_column_left']    = 'Kolom kiri';
+$_['text_column_right']   = 'Kolom kanan';
+
+// Entry
+$_['entry_creator']      = 'Dibuat oleh BIDatw.com';
+$_['entry_updated']      = 'Demo web pembuatnya :<br /><a href="https://www.bidatw.com/index.php?route=account/login" target="_blank">Klik</a>';
+$_['entry_version']      = 'Version:<br /><span class="help">1.0.1</span>';
+$_['entry_licence']      = 'Licence:<br /><span class="help"> www.BIDatw.com - All Rights Reserved</span>';
+$_['entry_apikey']        = 'Facebook API Key:';
+$_['entry_apisecret']     = 'Facebook API Secret:';
+$_['entry_pwdsecret']     = 'Password Encryption Key:';
+$_['entry_pwdsecret_desc']= 'Lebih aman menggunakan 4 sampai 8 Karakter (contoh. w7x4 ).';
+$_['entry_button']	  = 'Tombol koneksi:';
+$_['entry_button_desc']	  = 'Tulisan atau HTML.';
+$_['entry_layout']        = 'Layout:';
+$_['entry_position']      = 'Posisi:';
+$_['entry_status']        = 'Status:';
+$_['entry_sort_order']    = 'Urutan:';
+
+// Error
+$_['error_permission']    = 'Perhatian: Anda tidak bisa mengakses halaman ini!';
+$_['error_code']         = 'Salah: Facebook API Key, Facebook API Secret and Password Encryption Key harus di isi!';
+?>
